8bddea8f32753523cf59c5f3d3eff43960722878
Mike <myklee@gmail.com>
2025-06-22 02:41:18 +0800

    fix gh pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import FeedingsPage from '../views/FeedingsPage.vue'
 import BabyHistoryPage from '../views/BabyHistoryPage.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
