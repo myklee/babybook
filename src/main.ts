cc1e7149180d4b0b5b1cb320c1c24e422bed1fe1
myklee <myklee@gmail.com>
2025-06-27 01:31:39 +0800

    manually add ios to the body of the page

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,14 @@ import { createPinia } from 'pinia'
 import router from './router'
 import './style.css'
 import App from './App.vue'
+import { Capacitor } from '@capacitor/core'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+
+if (Capacitor.getPlatform && Capacitor.getPlatform() === 'ios') {
+  document.body.classList.add('ios')
+}
+
 app.mount('#app')
